6c706c1687fc825d8a825b1ca254c4a76a732051
liyuexin <1290821453@qq.com>
2016-09-12 16:24:16 +0800

    update

---
+++ src/main/java/com/yx/listener/AppListener.java
@@ -0,0 +1,24 @@
+package com.yx.listener; /**
+ * User: LiWenC
+ * Date: 16-9-12
+ */
+
+import com.yx.db.DBUtils;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener()
+public class AppListener implements ServletContextListener {
+
+    public AppListener() {
+    }
+
+    public void contextInitialized(ServletContextEvent sce) {
+        DBUtils.initDataSource();
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
